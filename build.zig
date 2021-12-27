d9a5ad38f9c92701e589f643cac4e7ce43b68cc9
iagocq <18238046+iagocq@users.noreply.github.com>
2021-12-27 03:44:28 -0300

    fix x86 windows cross compilation

--- build.zig
+++ build.zig
@@ -5,33 +5,12 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
-    const windows = b.option(bool, "windows", "build emulator for windows") orelse false;
-    const arch = b.option(Target.Cpu.Arch, "arch", "target architecture") orelse Target.Cpu.Arch.x86_64;
+    const target = b.standardTargetOptions(.{});
 
     const lib = b.addSharedLibrary("zemu", "src/main.zig", .unversioned);
+    lib.linkLibC();
     lib.setBuildMode(mode);
-
-    const os = init: {
-        const Os = Target.Os;
-        if (windows) {
-            break :init Os{ .tag = Os.Tag.windows, .version_range = .{ .windows = .{ .min = Os.WindowsVersion.xp, .max = Os.WindowsVersion.latest } } };
-        } else {
-            break :init Os.Tag.linux.defaultVersionRange(arch);
-        }
-    };
-
-    const abi = Target.Abi.default(arch, os);
-
-    const target = Target{
-        .cpu = Target.Cpu.baseline(arch),
-        .os = os,
-        .abi = abi,
-    };
-
-    if (windows) {
-        lib.setTarget(std.zig.CrossTarget.fromTarget(target));
-    }
-
+    lib.setTarget(target);
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
