dc7b29857511a2f8889e6de7440004d4518ddc2f
iagocq <18238046+iagocq@users.noreply.github.com>
2021-12-27 03:13:46 -0300

    add cross compilation options

--- build.zig
+++ build.zig
@@ -1,12 +1,37 @@
 const std = @import("std");
+const Target = std.Target;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    const windows = b.option(bool, "windows", "build emulator for windows") orelse false;
+    const arch = b.option(Target.Cpu.Arch, "arch", "target architecture") orelse Target.Cpu.Arch.x86_64;
 
-    const lib = b.addStaticLibrary("zemu", "src/main.zig");
+    const lib = b.addSharedLibrary("zemu", "src/main.zig", .unversioned);
     lib.setBuildMode(mode);
+
+    const os = init: {
+        const Os = Target.Os;
+        if (windows) {
+            break :init Os{ .tag = Os.Tag.windows, .version_range = .{ .windows = .{ .min = Os.WindowsVersion.xp, .max = Os.WindowsVersion.latest } } };
+        } else {
+            break :init Os.Tag.linux.defaultVersionRange(arch);
+        }
+    };
+
+    const abi = Target.Abi.default(arch, os);
+
+    const target = Target{
+        .cpu = Target.Cpu.baseline(arch),
+        .os = os,
+        .abi = abi,
+    };
+
+    if (windows) {
+        lib.setTarget(std.zig.CrossTarget.fromTarget(target));
+    }
+
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
