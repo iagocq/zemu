2fbde6842a2d168f0128bb5351291a797201fb58
iagocq <18238046+iagocq@users.noreply.github.com>
2021-12-30 01:36:08 -0300

    implement some types

--- src/steam_api.zig
+++ src/steam_api.zig
@@ -21,15 +21,32 @@ pub fn getImplFn(comptime struct_name: []const u8, comptime fn_name: []const u8,
 pub const size_t = usize;
 pub const SteamAPIWarningMessageHook_t = ?fn([*c]i32, [*c]const u8) callconv(.C) void;
 
+const GameID_t = packed struct {
+    m_nAppID: u24,
+    m_nType: u8,
+    m_nModID: u32,
+};
+
+pub const CGameID = extern union {
+    m_ulGameID: u64,
+    m_gameID: GameID_t,
+};
+
+const SteamIDComponent_t = packed struct {
+    m_unAccountID: u32,
+    m_unAccountInstance: u20,
+    m_EAccountType: u4,
+    m_EUniverse: u8,
+};
+
+pub const CSteamID = extern union {
+    m_unAll64Bits: u64,
+    m_comp: SteamIDComponent_t,
+};
+
 /////////////////////////////////////////
 // TODO implement these types properly //
 /////////////////////////////////////////
-
-pub const CGameID = u64;
-pub const CSteamID = extern union {
-    a: u64,
-    b: u32,
-};
 
 pub const SteamInputActionEvent_t = extern struct {
     _: u8,
